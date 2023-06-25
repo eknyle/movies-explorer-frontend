8703576ebbfd24b52c271c4de0598c406ff20e57
elena.lazareva <eknyle@yandex.ru>
2023-06-25 14:31:44 +0300

    Все страницы проверены на PP,Bem,W3

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -4,9 +4,11 @@ function Promo() {
   return (
     <section className="landing" aria-label="landing">
     <div className="landing__container">
-      <h1 className="landing__title">Учебный проект студента факультета Веб&#8209;разработки.
+      <h1 className="landing__title">Учебный проект студента факультета&#013;
+      Веб&#8209;разработки.
       </h1>
-      <p className="landing__description">Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+      <p className="landing__description">Листайте ниже, чтобы узнать больше про этот 
+      &#013;проект и его создателя.
       </p>
       <button className="landing__button" >Узнать больше</button>
       <div className="landing__logo"></div>
